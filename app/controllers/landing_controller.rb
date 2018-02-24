672f42b69ff2e2397d6eed3038e26f696ffe4022
Alex Xie <alexieyizhe@gmail.com>
2018-02-24 16:43:39 -0500

    testing code

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,7 @@
 class LandingController < ApplicationController
 	def index
 		@recipes = Recipe.order("RAND()").all[0..9] #randomize recipes shown on main page!
+		puts "OGH2EOIHGOEHGOEHWOIGHWEOIGHEOWGHEWIOGHWEHGIEWOHGOWGHWEIGWIHOGWEG"
+		puts @recipes
 	end
 end
