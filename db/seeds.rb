6f7d6af3e51477ddfd3d7f5a7509973e0abfc103
Alex Xie <alexieyizhe@gmail.com>
2018-02-24 16:51:33 -0500

    added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,14 @@
 
 Recipe.create(name: "Milk Steak", desc: "Scrumptious delicacy created by master chef Charlie Kelly. Best served with a warm glass of FIGHT MILK.", likes: 0, cooktime: 80, servings: 2, categories: ["gluten-free", "egg-free", "peanut-free", "fish-free", "high-protein", "low-carb"], ingredients: ["milk", "steak"], directions: nil, calories: 9428, img_url: "https://www.wikihow.com/images/thumb/1/1a/MIlk_steak_with_jelly_beans_373.jpg/900px-MIlk_steak_with_jelly_beans_373.jpg", source: "IASIP", orig_url: "http://itsalwayssunny.wikia.com/wiki/Milksteak")
 Recipe.create(name: "Rum Ham", desc: "A thanksgiving tradition started by the one and only Frank Reynolds.", likes: nil, cooktime: nil, servings: nil, categories: nil, ingredients: ["rum", "ham", "pork"], directions: nil, calories: nil, img_url: nil, source: nil, orig_url: nil)
-
+Recipe.create(name: "seed food1", desc: "seed1", likes: nil, cooktime: nil, servings: nil, categories: nil, ingredients: ["some food"], directions: nil, calories: nil, img_url: nil, source: nil, orig_url: nil)
+Recipe.create(name: "seed food2", desc: "seed2", likes: nil, cooktime: nil, servings: nil, categories: nil, ingredients: ["some food"], directions: nil, calories: nil, img_url: nil, source: nil, orig_url: nil)
+Recipe.create(name: "seed food3", desc: "seed1", likes: nil, cooktime: nil, servings: nil, categories: nil, ingredients: ["some food"], directions: nil, calories: nil, img_url: nil, source: nil, orig_url: nil)
+Recipe.create(name: "seed food4", desc: "seed1", likes: nil, cooktime: nil, servings: nil, categories: nil, ingredients: ["some food"], directions: nil, calories: nil, img_url: nil, source: nil, orig_url: nil)
+Recipe.create(name: "seed food5", desc: "seed1", likes: nil, cooktime: nil, servings: nil, categories: nil, ingredients: ["some food"], directions: nil, calories: nil, img_url: nil, source: nil, orig_url: nil)
+Recipe.create(name: "seed food6", desc: "seed1", likes: nil, cooktime: nil, servings: nil, categories: nil, ingredients: ["some food"], directions: nil, calories: nil, img_url: nil, source: nil, orig_url: nil)
+Recipe.create(name: "seed food7", desc: "seed1", likes: nil, cooktime: nil, servings: nil, categories: nil, ingredients: ["some food"], directions: nil, calories: nil, img_url: nil, source: nil, orig_url: nil)
+Recipe.create(name: "seed food8", desc: "seed1", likes: nil, cooktime: nil, servings: nil, categories: nil, ingredients: ["some food"], directions: nil, calories: nil, img_url: nil, source: nil, orig_url: nil)
+Recipe.create(name: "seed food9", desc: "seed1", likes: nil, cooktime: nil, servings: nil, categories: nil, ingredients: ["some food"], directions: nil, calories: nil, img_url: nil, source: nil, orig_url: nil)
+Recipe.create(name: "seed food10", desc: "seed1", likes: nil, cooktime: nil, servings: nil, categories: nil, ingredients: ["some food"], directions: nil, calories: nil, img_url: nil, source: nil, orig_url: nil)
+Recipe.create(name: "seed food11", desc: "seed1", likes: nil, cooktime: nil, servings: nil, categories: nil, ingredients: ["some food"], directions: nil, calories: nil, img_url: nil, source: nil, orig_url: nil)
