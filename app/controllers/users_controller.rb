46179b92242a1f0604901c1dcbde3037c6a4e618
Alex Xie <alexieyizhe@gmail.com>
2018-02-24 20:47:34 -0500

    w

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,6 @@ class UsersController < ApplicationController
 
 	private
 		def user_params
-			params.require(:user).permit(:name, :email, :bio, :password, :password_confirmation)
+			params.require(:user).permit(:name, :email, :bio, :password, :role, :password_confirmation)
 		end
 end
